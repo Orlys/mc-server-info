e3ec1dd072c4c93c7523891b6d5c8a9fe5e6068e
ᘡ˚ <viyrex.aka.yuyu@gmail.com>
2020-04-21 08:37:45 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,17 +1,57 @@
+# encoding: utf-8
+import os
 from mcstatus import MinecraftServer
 import discord
 from discord.ext import commands
 
-server = MinecraftServer.lookup("127.0.0.1:25565")
+MC_SERVER_ENDPOINT = '127.0.0.1:25565'
+MC_BOT_OAUTH20_TOKEN = os.getenv('MC_BOT_OAUTH20_TOKEN')
+if MC_BOT_OAUTH20_TOKEN is None:
+    exit(-1)
+
+server = MinecraftServer.lookup(MC_SERVER_ENDPOINT)
 bot = commands.Bot(command_prefix='@')
 
-@bot.command()
-async def 誰在玩(ctx):
-    s = ''
-    for p in server.status().players.sample:
-        s += p.name + '\r\n'
-    await ctx.send(content=s)
+@bot.event
+async def on_ready():
+    print('init completed.')
+    print('mention: '+bot.user.mention)
 
-bot.run('<your OAuth 2.0 token here>')
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return 
+    # print('>'+message.content+'<')
+    if message.content in [ bot.user.mention, f'<@!{bot.user.id}>']:
+        try:
+            players = server.status().players.sample
+            print(players)
+            if players == None:
+                await message.channel.send(content='現在沒人在玩唷 uwu)/')
+            else:
+                s = '正在玩的人有: \r\n'
+                for p in server.status().players.sample:
+                    s += p.name + '\r\n'
+                await message.channel.send(content=s)
+        
+        except:
+            await message.channel.send(content='伺服器可能沒開 \\(qwq')
+    #if message.content == bot.user.name:
+    #    await message.channel.send('???')
 
-input()
+#@bot.command()
+#async def 誰在玩(ctx):
+#    try:
+#        s = ''
+#        for p in server.status().players.sample:
+#            s += p.name + '\r\n'
+    
+#        if len(s) == 0:
+#            await ctx.send(content='現在沒人在玩唷 uwu)/')
+#        else:
+#            await ctx.send(content=s)
+#    except:
+#        await ctx.send(content='伺服器可能沒開 \(qwq')
+    
+
+bot.run(MC_BOT_OAUTH20_TOKEN)
